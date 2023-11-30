2f70716b8a3a4439924f9ec2971451b2a51c82c3
Thiago Oliveira <thiago.oliveira.ds18@gmail.com>
2023-11-30 18:25:46 -0300

    Backend: Inserção do WebSocket e SocketIo

---
+++ backend/src/gateway/gateway.ts
@@ -0,0 +1,38 @@
+import { OnModuleInit } from '@nestjs/common';
+import {
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway()
+export class MyGateway
+  implements OnModuleInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() server: Server;
+
+  onModuleInit() {
+    this.server.on('connection', () => {
+      console.log('Succesful Connected.');
+    });
+  }
+
+  handleConnection(client: Socket) {
+    console.log(`User: ${client.id} connected.`);
+  }
+  handleDisconnect(client: Socket) {
+    console.log(`User: ${client.id} disconnected.`);
+  }
+
+  @SubscribeMessage('newMessage')
+  onNewMessage(@MessageBody() body: any): void {
+    this.server.emit('onMessage', {
+      msg: 'newMessage',
+      content: body,
+    });
+  }
+}
