6839608feb2c11a7a43da2c8636f250c7211502f
Thiago Oliveira <thiago.oliveira.ds18@gmail.com>
2023-11-30 18:59:38 -0300

    Backend: Limpando arquivos

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GatewayModule } from './gateway/gateway.module';
 
 @Module({
   imports: [GatewayModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
