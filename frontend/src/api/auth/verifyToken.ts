1c1fa25f32ae9fd97b890ca47e79434c93b20470
Thiago Oliveira <thiago.oliveira.ds18@gmail.com>
2023-12-05 18:13:11 -0300

    Frontend: Criado o metodo de verificacao de Token e Implementado metodo de verificacao na Page Index do projeto

---
+++ frontend/src/api/auth/verifyToken.ts
@@ -0,0 +1,12 @@
+import instance from "..";
+
+export default async function verifyToken(token: string) {
+    try {
+        const valid = await instance.post('auth/profile', {
+            "token": token
+        });
+        return valid.data;
+    } catch(err) {
+        throw new Error('Falha na comunicação.');
+    }
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,38 @@
-import Image from 'next/image'
+'use client';
+
+import verifyToken from "@/api/auth/verifyToken";
+
+import getCookie from "@/api/cookies/getCookie";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return (
-   <>Hello World!</>
+  const router = useRouter();
+
+  async function verifyPermission() {
+    try{
+      const token = await getCookie();
+      if(!token){
+          throw new Error('token inválido.');
+      }
+      const permission = await verifyToken(token.toString());
+      if (!permission) {
+        router.push('/login');
+      }
+      return;
+    } catch(err){
+      router.push('/login');
+    }
+  }
+
+  useEffect( () => {
+    router.push('/login');
+  });
+
+
+  return (    
+    <>
+      <p className="text-3xl font-black">Redirect...</p>
+    </>
   )
 }
